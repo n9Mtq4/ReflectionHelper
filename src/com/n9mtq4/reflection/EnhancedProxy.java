6891504635aa3f6155a491b274c1f03621ffdf0b
n9Mtq4 <bill.wresnahan@gmail.com>
2015-08-05 16:30:44 -0400

    added support for proxies

---
+++ src/com/n9mtq4/reflection/EnhancedProxy.java
@@ -0,0 +1,37 @@
+package com.n9mtq4.reflection;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by will on 8/5/15 at 4:13 PM.
+ */
+public class EnhancedProxy implements InvocationHandler {
+	
+	public static <E> E newInstance(Object obj, EnhancedInvocationHandler handler) {
+		return (E) Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), new EnhancedProxy(obj, handler));
+	}
+	
+	private Object obj;
+	EnhancedInvocationHandler handler;
+	
+	public EnhancedProxy(Object obj, EnhancedInvocationHandler handler) {
+		this.obj = obj;
+		this.handler = handler;
+	}
+	
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		
+		return handler.invoke(obj, proxy, method, args);
+		
+	}
+	
+	public interface EnhancedInvocationHandler {
+		
+		Object invoke(Object obj, Object proxy, Method method, Object[] args);
+		
+	}
+	
+}
